830ba63efa840a7703aa49dfe9732574dd96e7ae
Lachy Shinnick <shinnicklachlan@gmail.com>
2025-06-20 07:59:47 +0000

    [automatic commit] 2025-06-20 07:59:47 UTC 110.175.29.111

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -65,7 +65,7 @@ router.post('/login', async (req, res) => {
 
 router.post('/logout', (req, res) => {
   req.session.destroy(err => {
-    
+    if (err) return res.status(500).json({ error})
   })
 })
 
