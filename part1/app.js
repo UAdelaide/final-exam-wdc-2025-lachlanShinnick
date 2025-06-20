6ada1326dc94f87347a81948d7e4614c7377d595
Lachy Shinnick <shinnicklachlan@gmail.com>
2025-06-20 03:09:39 +0000

    [automatic commit] 2025-06-20 03:09:39 UTC 110.175.29.111

--- part1/app.js
+++ part1/app.js
@@ -1,5 +1,6 @@
 
 
 async function seed() {
-    const sql = fs.readFileSync(path.join(__dirname, 'dogwalks.sql'), 'utf8')
+    const sql = fs.readFileSync(path.join(__dirname, 'dogwalks.sql'), 'utf8');
+    await pool.query(sql);
 }
