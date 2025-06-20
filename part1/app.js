ab3da7c7d982b83975ae4d95d18f3a78e4c82746
Lachy Shinnick <shinnicklachlan@gmail.com>
2025-06-20 03:30:21 +0000

    [automatic commit] 2025-06-20 03:30:21 UTC 110.175.29.111

--- part1/app.js
+++ part1/app.js
@@ -38,8 +38,10 @@ async function main() {
                 FROM WalkRequests wr
                 JOIN Dogs d ON wr.dog_id = d.dog_id
                 JOIN Users u ON d.owner_id = u.user_id
-                WHERE wr.status = 
-            `)
+                WHERE wr.status = 'open'
+            `);
+            res.json(rows);
+            
         }
     })
 
