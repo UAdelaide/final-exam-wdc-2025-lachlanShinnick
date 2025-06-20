555004c635e2ead26756d7b0012187899a6a90bd
Lachy Shinnick <shinnicklachlan@gmail.com>
2025-06-20 04:33:33 +0000

    [automatic commit] 2025-06-20 04:33:33 UTC 110.175.29.111

--- part1/app.js
+++ part1/app.js
@@ -60,7 +60,7 @@ async function main() {
                             AND wa.status = 'accepted'
                             THEN 1 ELSE 0
                         END
-                    ) AS completed_walks
+                    ) + 0 AS completed_walks
                 FROM Users u
                 LEFT JOIN WalkRatings r
                     ON u.user_id = r.walker_id
