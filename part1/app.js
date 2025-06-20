f4deb0d4e0f4fafb540997831069b8bd6cdaa87d
Lachy Shinnick <shinnicklachlan@gmail.com>
2025-06-20 04:35:35 +0000

    [automatic commit] 2025-06-20 04:35:35 UTC 110.175.29.111

--- part1/app.js
+++ part1/app.js
@@ -53,8 +53,8 @@ async function main() {
                 SELECT
                     u.username AS walker_username,
                     CAST(COUNT(r.rating) AS UNSIGNED) AS total_ratings,
-                    CAST(AVG(r.rating) AS DECIMAL(10,2) AS average_rating,
-                    SUM(
+                    CAST(AVG(r.rating) AS DECIMAL(10,2)) AS average_rating,
+                    CAST(SUM(
                         CASE
                             WHEN wr.status = 'completed'
                             AND wa.status = 'accepted'
