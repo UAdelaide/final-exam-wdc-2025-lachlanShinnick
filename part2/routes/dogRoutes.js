b73aa320d8725f160b0dacd96a91ec4c9dc8dcd7
Lachy Shinnick <shinnicklachlan@gmail.com>
2025-06-20 08:47:41 +0000

    [automatic commit] 2025-06-20 08:47:41 UTC 110.175.29.111

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models.db');
 
-//GET /api/owners/:ownerId/
+//GET /api/owners/:ownerId/dogs
+
+router.get('owners/;ownerId/dogs', as)
