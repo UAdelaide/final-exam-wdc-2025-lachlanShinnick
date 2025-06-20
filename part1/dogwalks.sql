6136c822d4c8f2482c9760300a6f4fa7f118391c
Lachy Shinnick <shinnicklachlan@gmail.com>
2025-06-20 02:55:02 +0000

    [automatic commit] 2025-06-20 02:55:02 UTC 110.175.29.111

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -100,6 +100,13 @@ INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, st
         (SELECT dog_id FROM Dogs WHERE name = 'Bella'),
         '2025-06-10 09:30:00',
         45,
-        'Be',
+        'Beachside Ave',
+        'accepted'
+    ),
+    (
+        (SELECT dog_id FROM Dogs WHERE name = 'MichealJordan'),
+        '2025-06-10 08:00:00',
+        30,
+        'Parklands',
         'open'
     ),
