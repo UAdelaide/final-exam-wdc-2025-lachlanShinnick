859b53923c92c767e8030c90c70c1d966ad6d785
Lachy Shinnick <shinnicklachlan@gmail.com>
2025-06-20 08:52:47 +0000

    [automatic commit] 2025-06-20 08:52:47 UTC 110.175.29.111

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -9,12 +9,12 @@ router.get('owners/:ownerId/dogs', async (req, res => {
 
     try {
         const [rows] = await db.query(
-            'SELECT dog_id, name, size, FROM Dogs WHERE owner_id = ?',
+            'SELECT dog_id, name, size FROM Dogs WHERE owner_id = ?',
             [ownerId]
         );
         res.json(rows);
     } catch (err) {
-        res.status(500.json({ error: 'Failed to load dogs' }));
+        res.status(500).json({ error: 'Failed to load dogs' }));
     }
 }));
 
